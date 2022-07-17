58c6d3c8993072dec5cb0aecd9e89ad39929e5d2
mmarcell264 <m.marcell264@gmail.com>
2022-07-17 18:45:30 +0200

    Test server status API endpoint

--- backend/key_value_server/key_value_API/tests.py
+++ backend/key_value_server/key_value_API/tests.py
@@ -1,6 +1,7 @@
 from key_value_API import service
 from rest_framework.test import APITestCase
 from rest_framework.exceptions import APIException
+from django.urls import reverse
 
 # Create your tests here.
 
@@ -38,3 +39,12 @@ class TestKeyValueSerializer(APITestCase):
     def test_none_existing_field(self):
 
         self.assertRaises(APIException, service.key_value_serialization_or_500, fields=('none_existent',))
+
+
+class TestServerStatus(APITestCase):
+
+    def test_server_is_up(self):
+        url = reverse("key_value_API:server_status")
+        response = self.client.get(url, format="json")
+
+        self.assertEqual(response.data, {"status": "Server is running."})
