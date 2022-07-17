6d9c5ba73fb98379156c1f11d8831d4ae0b974a6
mmarcell264 <m.marcell264@gmail.com>
2022-07-17 15:59:43 +0200

    Add postgresql connection

--- backend/key_value_server/key_value_server/settings.py
+++ backend/key_value_server/key_value_server/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'key_value_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'key_value_db',
+        'USER': 'keyvalue',
+        'PASSWORD': os.getenv('KEYVALUE_PW'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
