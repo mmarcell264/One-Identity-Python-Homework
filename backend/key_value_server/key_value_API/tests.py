100cfc4f033f284ed6fffd15a08309ce6c585b1e
mmarcell264 <m.marcell264@gmail.com>
2022-07-17 19:13:01 +0200

    Add key value add API endpoint tests

--- backend/key_value_server/key_value_API/tests.py
+++ backend/key_value_server/key_value_API/tests.py
@@ -1,7 +1,9 @@
+from .models import KeyValue
 from key_value_API import service
 from rest_framework.test import APITestCase
 from rest_framework.exceptions import APIException
 from django.urls import reverse
+from rest_framework import status
 
 # Create your tests here.
 
@@ -48,3 +50,138 @@ class TestServerStatus(APITestCase):
         response = self.client.get(url, format="json")
 
         self.assertEqual(response.data, {"status": "Server is running."})
+
+
+class TestAddKeyValue(APITestCase):
+
+    def setUp(self):
+        key_value = KeyValue(key="window", value="big")
+        key_value.save()
+
+    def test_wrong_input_type(self):
+        url = reverse("key_value_API:add_key_and_value")
+
+        data_list = [{"key": "trial_key1", "value": "trial_value1"},
+                {"key": "trial_key2", "value": "trial_value2"}]
+        data_bare = 10
+
+        response_list = self.client.post(url, data=data_list,format="json")
+
+        self.assertEqual(response_list.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response_list.data, {"error": "Not valid input."})
+        self.assertEqual(KeyValue.objects.count(), 1)
+
+        response_bare = self.client.post(url, data=data_bare, format="json")
+
+        self.assertEqual(response_bare.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response_bare.data, {"error": "Not valid input."})
+        self.assertEqual(KeyValue.objects.count(), 1)
+
+    def test_too_few_fields(self):
+        url = reverse("key_value_API:add_key_and_value")
+
+        data = {}
+
+        response = self.client.post(url, data=data,format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {"error": "Too few input fields."})
+        self.assertEqual(KeyValue.objects.count(), 1)
+
+    def test_too_many_fields(self):
+        url = reverse("key_value_API:add_key_and_value")
+
+        data = {"a": "a", "b": "b" , "c": "c"}
+
+        response = self.client.post(url, data=data,format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {"error": "Too many input fields."})
+        self.assertEqual(KeyValue.objects.count(), 1)
+
+    def test_missing_key_field(self):
+        url = reverse("key_value_API:add_key_and_value")
+
+        data = {"a": "trial_key", "value": "trial_value"}
+
+        response = self.client.post(url, data=data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data,{"error": "Missing key or value field."})
+        self.assertEqual(KeyValue.objects.count(), 1)
+
+    def test_missing_value_field(self):
+        url = reverse("key_value_API:add_key_and_value")
+
+        data = {"key": "trial_key", "b": "trial_value"}
+
+        response = self.client.post(url, data=data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {"error": "Missing key or value field."})
+        self.assertEqual(KeyValue.objects.count(), 1)
+
+    def test_missing_key_value_field(self):
+        url = reverse("key_value_API:add_key_and_value")
+
+        data = {"a": "trial_key", "b": "trial_value"}
+
+        response = self.client.post(url, data=data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {"error": "Missing key or value field."})
+        self.assertEqual(KeyValue.objects.count(), 1)
+
+    def test_wrong_key_field_type(self):
+        url = reverse("key_value_API:add_key_and_value")
+
+        data = {"key": 5, "value": "trial_value"}
+
+        response = self.client.post(url, data=data,format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {"error": "The key's and value's value should be string."})
+        self.assertEqual(KeyValue.objects.count(), 1)
+
+    def test_wrong_value_field_type(self):
+        url = reverse("key_value_API:add_key_and_value")
+
+        data = {"key": "trial_key", "value": 5}
+
+        response = self.client.post(url, data=data,format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {"error": "The key's and value's value should be string."})
+        self.assertEqual(KeyValue.objects.count(), 1)
+
+    def test_wrong_key_value_field_type(self):
+        url = reverse("key_value_API:add_key_and_value")
+
+        data = {"key": 5, "value": 10}
+
+        response = self.client.post(url, data=data,format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {"error": "The key's and value's value should be string."})
+        self.assertEqual(KeyValue.objects.count(), 1)
+
+    def test_add_same_key_with_different_value(self):
+        url = reverse("key_value_API:add_key_and_value")
+
+        data = {"key": "window", "value": "big"}
+
+        response = self.client.post(url, data=data,format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(KeyValue.objects.count(), 1)
+
+    def test_successful_add(self):
+        url = reverse("key_value_API:add_key_and_value")
+
+        data = {"key": "trial_key", "value": "trial_value"}
+
+        response = self.client.post(url, data=data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, {"status": "Key-value pair has been successfully created."})
+        self.assertEqual(KeyValue.objects.count(), 2)
