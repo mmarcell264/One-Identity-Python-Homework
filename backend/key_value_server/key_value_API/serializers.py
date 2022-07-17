11eecf5d8bce8af0bacbf605cf1957c28454fdb0
mmarcell264 <m.marcell264@gmail.com>
2022-07-17 16:22:12 +0200

    Add key value serializer

---
+++ backend/key_value_server/key_value_API/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import KeyValue
+
+
+class KeyValueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = KeyValue
+        fields = ["key", "value"]
+
