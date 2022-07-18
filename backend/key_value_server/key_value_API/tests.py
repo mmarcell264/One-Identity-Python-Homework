bdcee8f863fbacd9dbb8d8ee6db81fb11ec4c21a
mmarcell264 <m.marcell264@gmail.com>
2022-07-18 18:28:17 +0200

    Update previous tests with auth and add CustomAuthToken API endpoint tests

--- backend/key_value_server/key_value_API/tests.py
+++ backend/key_value_server/key_value_API/tests.py
@@ -1,9 +1,13 @@
 from .models import KeyValue
+from key_value_API import views
 from key_value_API import service
 from rest_framework.test import APITestCase
 from rest_framework.exceptions import APIException
-from django.urls import reverse
 from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
 
 # Create your tests here.
 
@@ -52,26 +56,57 @@ class TestServerStatus(APITestCase):
         self.assertEqual(response.data, {"status": "Server is running."})
 
 
+class TestCustomAuthToken(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test', password='test')
+
+    def test_not_valid_username_password(self):
+        url = reverse("key_value_API:token_auth")
+
+        data = {"username": "test", "password": "password"}
+
+        response= self.client.post(url, data=data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_get_token(self):
+        url = reverse("key_value_API:token_auth")
+
+        data = {"username": "test", "password": "test"}
+
+        response= self.client.post(url, data=data, format='json')
+
+        self.assertTrue(response.data['token'])
+
+
 class TestAddKeyValue(APITestCase):
 
     def setUp(self):
         key_value = KeyValue(key="window", value="big")
         key_value.save()
 
+        self.user = User.objects.create_user(
+            username='test',  password='test')
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+        self.token = f"Bearer {self.token}"
+
     def test_wrong_input_type(self):
         url = reverse("key_value_API:add_key_and_value")
 
         data_list = [{"key": "trial_key1", "value": "trial_value1"},
-                {"key": "trial_key2", "value": "trial_value2"}]
+                    {"key": "trial_key2", "value": "trial_value2"}]
         data_bare = 10
 
-        response_list = self.client.post(url, data=data_list,format="json")
+        response_list = self.client.post(url, data=data_list, format='json', HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response_list.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response_list.data, {"error": "Not valid input."})
         self.assertEqual(KeyValue.objects.count(), 1)
 
-        response_bare = self.client.post(url, data=data_bare, format="json")
+        response_bare = self.client.post(url, data=data_bare, format='json', HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response_bare.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response_bare.data, {"error": "Not valid input."})
@@ -82,7 +117,7 @@ class TestAddKeyValue(APITestCase):
 
         data = {}
 
-        response = self.client.post(url, data=data,format="json")
+        response = self.client.post(url, data=data, format="json", HTTP_AUTHORIZATION=self.token,)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, {"error": "Too few input fields."})
@@ -93,7 +128,7 @@ class TestAddKeyValue(APITestCase):
 
         data = {"a": "a", "b": "b" , "c": "c"}
 
-        response = self.client.post(url, data=data,format="json")
+        response = self.client.post(url, data=data, format="json", HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, {"error": "Too many input fields."})
@@ -104,7 +139,7 @@ class TestAddKeyValue(APITestCase):
 
         data = {"a": "trial_key", "value": "trial_value"}
 
-        response = self.client.post(url, data=data, format="json")
+        response = self.client.post(url, data=data, format="json", HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data,{"error": "Missing key or value field."})
@@ -115,7 +150,7 @@ class TestAddKeyValue(APITestCase):
 
         data = {"key": "trial_key", "b": "trial_value"}
 
-        response = self.client.post(url, data=data, format="json")
+        response = self.client.post(url, data=data, format="json", HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, {"error": "Missing key or value field."})
@@ -126,7 +161,7 @@ class TestAddKeyValue(APITestCase):
 
         data = {"a": "trial_key", "b": "trial_value"}
 
-        response = self.client.post(url, data=data, format="json")
+        response = self.client.post(url, data=data, format="json", HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, {"error": "Missing key or value field."})
@@ -137,7 +172,7 @@ class TestAddKeyValue(APITestCase):
 
         data = {"key": 5, "value": "trial_value"}
 
-        response = self.client.post(url, data=data,format="json")
+        response = self.client.post(url, data=data, format="json", HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, {"error": "The key's and value's value should be string."})
@@ -148,7 +183,7 @@ class TestAddKeyValue(APITestCase):
 
         data = {"key": "trial_key", "value": 5}
 
-        response = self.client.post(url, data=data,format="json")
+        response = self.client.post(url, data=data, format="json", HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, {"error": "The key's and value's value should be string."})
@@ -159,7 +194,7 @@ class TestAddKeyValue(APITestCase):
 
         data = {"key": 5, "value": 10}
 
-        response = self.client.post(url, data=data,format="json")
+        response = self.client.post(url, data=data, format="json", HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, {"error": "The key's and value's value should be string."})
@@ -170,7 +205,7 @@ class TestAddKeyValue(APITestCase):
 
         data = {"key": "window", "value": "big"}
 
-        response = self.client.post(url, data=data,format="json")
+        response = self.client.post(url, data=data, format="json", HTTP_AUTHORIZATION=self.token,)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(KeyValue.objects.count(), 1)
@@ -180,7 +215,7 @@ class TestAddKeyValue(APITestCase):
 
         data = {"key": "trial_key", "value": "trial_value"}
 
-        response = self.client.post(url, data=data, format="json")
+        response = self.client.post(url, data=data, format="json", HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, {"status": "Key-value pair has been successfully created."})
@@ -192,17 +227,23 @@ class TestGetValueByKey(APITestCase):
         key_value = KeyValue(key="window", value="big")
         key_value.save()
 
+        self.user = User.objects.create_user(
+            username='test', password='test')
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+        self.token = f"Bearer {self.token}"
+
     def test_key_not_found(self):
         url = reverse("key_value_API:get_value_by_key", kwargs={"key": "trial"})
 
-        response = self.client.get(url, format="json")
+        response = self.client.get(url, format="json", HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_key_found(self):
         url = reverse("key_value_API:get_value_by_key", kwargs={"key": "window"})
 
-        response = self.client.get(url, format='json')
+        response = self.client.get(url, format='json', HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response.data, {"value": "big"})
 
@@ -215,10 +256,16 @@ class TestGetKeysByValuePrefix(APITestCase):
         KeyValue.objects.create(key="boring", value="wicky")
         KeyValue.objects.create(key="distinct", value="Widdy")
 
+        self.user = User.objects.create_user(
+            username='test', password='test')
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+        self.token = f"Bearer {self.token}"
+
     def test_missing_prefix_parameter(self):
         url = reverse("key_value_API:get_keys_by_value_prefix")
 
-        response = self.client.get(url, format="json")
+        response = self.client.get(url, format="json", HTTP_AUTHORIZATION=self.token)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, {"error": "Please use the prefix parameter."})
@@ -226,6 +273,6 @@ class TestGetKeysByValuePrefix(APITestCase):
     def test_with_prefix_parameter(self):
         url = reverse("key_value_API:get_keys_by_value_prefix")
 
-        response = self.client.get(url, {"prefix": 'wi'},format="json")
+        response = self.client.get(url, {"prefix": 'wi'},format="json", HTTP_AUTHORIZATION=self.token)
 
-        self.assertEqual(len(response.data), 4)
+        self.assertEqual(response.data["count"], 4)
--- backend/key_value_server/key_value_API/urls.py
+++ backend/key_value_server/key_value_API/urls.py
@@ -5,7 +5,7 @@ app_name = "key_value_API"
 
 urlpatterns = [
     path("", views.server_status, name="server_status"),
-    path('token-auth/', views.CustomAuthToken.as_view()),
+    path('token-auth/', views.CustomAuthToken.as_view(), name="token_auth"),
     path("add/", views.add_key_value, name="add_key_and_value"),
     path("get/<key>/value/", views.get_value_by_key ,name="get_value_by_key"),
     path("get/keys/", views.get_keys_by_value_prefix, name="get_keys_by_value_prefix"),
