60fe492889f22c2ba508d27c4bc70c595f255b61
mmarcell264 <m.marcell264@gmail.com>
2022-07-19 15:39:04 +0200

    Add install softwares script

---
+++ installation_and_setup/install_softwares.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Update
+sudo apt update
+
+#Python3 softwares
+sudo apt install python3
+sudo apt install python3-venv
+
+#Postgesql
+sudo apt install postgresql
+
+#Test API
+sudo apt install httpie
